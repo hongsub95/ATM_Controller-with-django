566a54f5063b153b477552b34ce1557604c0966a
hongsub <hong9506@gmail.com>
2022-12-01 17:43:00 +0900

    initial data

--- Accounts/tests.py
+++ Accounts/tests.py
@@ -2,20 +2,5 @@ from django.test import TestCase
 from django.test import Client
 from . import models
 
-class AtmTest(TestCase):
-    def setUp(self) -> None:
-        account1 = models.AccountInfo.objects.create(account_number='1234567890',balance=300)
-        account2 = models.AccountInfo.objects.create(account_number='1111111111',balance=240)
-        account3 = models.AccountInfo.objects.create(account_number='2222222222',balance=100)
-        account4 = models.AccountInfo.objects.create(account_number='3333333333',balance=500)
-        account5 = models.AccountInfo.objects.create(account_number='0123456789',balance=30)
-        card1 = models.CardInfo.objects.create(card_number='1111222233334444',account_number=models.AccountInfo.objects.get(pk=account1.pk),pin="1234",name='a')
-        card2 = models.CardInfo.objects.create(card_number='2222333344445555',account_number=models.AccountInfo.objects.get(pk=account2.pk),pin="1234",name='b')
-        card3 = models.CardInfo.objects.create(card_number='3333444455556666',account_number=models.AccountInfo.objects.get(pk=account3.pk),pin="1234",name='c')
-        card4 = models.CardInfo.objects.create(card_number='4444555566667777',account_number=models.AccountInfo.objects.get(pk=account4.pk),pin="1234",name='d')
-        card5 = models.CardInfo.objects.create(card_number='5555666677778888',account_number=models.AccountInfo.objects.get(pk=account5.pk),pin="1234",name='e')
-    def test_cash_deposit(self):
-        c=Client()
-        res=c.post('account:deposit')
-        self.assertEqual(res.status_code,200)
+
         
--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # ATM_Controller-with-django
 
 
-virtualenv : poetry
+virtualenv : poetry\
 web-framework : django
 
-Have to do :
-[] Insert Card  => Instead of Insert card , Check Card Number
-[] Select Account + PIN number  => Check Account number and PIN number (valid or not)
-[] See Balance/Deposit/Withdraw 
+Have to do :\
+[o] Insert Card  => click Insert card \
+[o] Select Account + PIN number  => Check Card number and PIN number (valid or not)\
+[o] Select transaction(Balance/Deposit/Withdraw) \
+[o] Balance Inquiry, Deposit view, Withdraw view
 
 
-db_model : AccountInfo(계좌정보), CardInfo(카드정보)
-Account_schema : account_number(계좌번호), balance(현재잔액)
+db_model : AccountInfo(계좌정보), CardInfo(카드정보)\
+Account_schema : account_number(계좌번호), balance(현재잔액)\
 Card_chema : Account_id, name(사용자정보), card_number(카드번호), Pin(핀넘버), phone_number(사용자 전화번호)
 
 
